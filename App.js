f73c6bc4770ad647f027e7d4ca6a7e474dd39779
Ba5eem <baseem@gmail.com>
2017-12-19 13:56:47 -1000

    Redux setup with react native"

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Header, CryptoContainer } from './src/components';
+
+import { Provider } from 'react-redux';
+import Store from './src/Store';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={Store}>
+        <View>
+          <Header/>
+          <CryptoContainer/>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
