a54bee96b12a844ee01fd72fb864ec7bdf1cddfe
Ba5eem <baseem@gmail.com>
2017-12-20 09:36:05 -1000

    fetching crypto data from api

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text, StyleSheet } from 'react-native';
+import FetchCoinData from '../Actions/FetchCoinData';
 
 class CryptoContainer extends Component {
+
+
+  componentDidMount() {
+    this.props.FetchCoinData()
+  }
+
   render() {
     return(
         <View>
@@ -21,4 +28,5 @@ function mapStateToProps(state){
   }
 }
 
-export default connect(mapStateToProps)(CryptoContainer);
+
+export default connect(mapStateToProps,{FetchCoinData})(CryptoContainer);
